1915aba2a6b39b842cc070fc740dc1e304a87960
IR~SAGE <sibo.egide98@gmail.com>
2020-07-26 18:34:25 +0200

    Making The Home Screen Dynamic Using React

---
+++ Frontend/src/data.js
@@ -0,0 +1,49 @@
+export default  {
+    products: [
+        {
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Misozi',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        name:'Fit Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:40,
+        brand:'Misozi',
+        rating: 3.5,
+        numReviews:4
+    },
+    {
+        name:'Slim Pants',
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price:80,
+        brand:'Misozi',
+        rating: 5.5,
+        numReviews:6
+    },
+    {
+        name:'Flesh Pants',
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price:20,
+        brand:'Misozi',
+        rating: 9.5,
+        numReviews:20
+    },
+    {
+        name:'Flesh Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Misozi',
+        rating: 4.5,
+        numReviews:5
+    }
+]
+}
