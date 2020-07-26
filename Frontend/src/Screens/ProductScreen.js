eb2563cdc2b58f6b53b7d4a571aa66d7a2386032
IR~SAGE <sibo.egide98@gmail.com>
2020-07-26 20:48:22 +0200

    Designing The Product Details Page

--- Frontend/src/Screens/ProductScreen.js
+++ Frontend/src/Screens/ProductScreen.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import data from '../data';
+import { Link } from 'react-router-dom';
 
 
 function ProductScreen(props) {
-    return  <div>ProductScreen</div>
+    console.log(props.match.params.id);
+    const product = data.products.find ( x  => x._id === props.match.params.id);
+    return  <div>
+        <div className="back-to-results"> <Link to="/">Back to results</Link></div>
+        <div className="details">
+        <div className= "details-image">
+          <img src={product.image} alt="Product"></img>
+        </div>
+        <div className="details-info">
+            <ul>
+                <li><h4>{product.name}</h4></li>
+                <li>{product.rating} Stars ({product.numReviews} Reviews)</li>
+                <li><b>{product.price}</b></li>
+                <li>Description: <div>{product.description}</div></li>
+            </ul>
+        </div>
+        <div className="details-action">
+            <ul>
+                <li>Price:${product.price}</li>
+                <li>status:{product.status}</li>
+                <li>Qty:<select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    </select></li>
+                    <li><button className ="button primary">Add To Cart</button></li>
+            </ul>
+        </div>
+        </div>
+        
+    </div>
 }
 
 
