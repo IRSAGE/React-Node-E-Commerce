9df8bb9bf1e4532e5b37f13181092c5e0fdfffd1
IR~SAGE <sibo.egide98@gmail.com>
2020-07-28 10:37:06 +0200

    Initialising the backend Server

---
+++ Backend/data.js
@@ -0,0 +1,44 @@
+export default  {
+    products: [
+        {
+        _id:'1',
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Misozi',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        _id:'2',
+        name:'Fit Shirt',
+        category:'Shirts',
+        image:'/images/d2.jpg',
+        price:40,
+        brand:'Misozi',
+        rating: 3.5,
+        numReviews:4
+    },
+    {
+        _id:'3',
+        name:'Slim Pants',
+        category:'Pants',
+        image:'/images/d3.jpg',
+        price:80,
+        brand:'Misozi',
+        rating: 5.5,
+        numReviews:6
+    },
+    {
+        _id:'4',
+        name:'Flesh Pants',
+        category:'Pants',
+        image:'/images/d4.jpg',
+        price:20,
+        brand:'Misozi',
+        rating: 9.5,
+        numReviews:20
+    }
+]
+}
